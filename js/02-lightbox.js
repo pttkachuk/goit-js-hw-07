ba47df875fa121b596a45a372e73e18b212da07e
pttkachuk <petrotkachuk6@gmail.com>
2023-06-07 16:06:25 +0200

    create gallery with navigations arrows

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const cardList = document.querySelector(".gallery");
+
+function createCardList(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) =>
+        `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`
+    ).join('');
+}
+cardList.insertAdjacentHTML('beforeend', createCardList(galleryItems));
+
+new SimpleLightbox('.gallery__link', {
+    captionsData: 'alt',
+    captionDelay: 250,
+});
